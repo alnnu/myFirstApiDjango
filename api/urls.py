7ef2223baca94a916b95a8124b6dbd07c00cb173
alnnu <luannf1020@gmail.com>
2024-09-18 18:27:01 -0300

    add: get user, delete user, uptade user

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import getUsers, createUser
+from .views import getUsers, getUser, createUser, updateUser, deleteUser
 
 
 urlpatterns = [
     path('users/', getUsers, name="getUsers"),
     path('users/create', createUser, name="createUser"),
+    path('users/<int:pk>/', getUser, name="getUser"),
+    path('users/<int:pk>/update', updateUser, name="updateUser"),
+    path('users/<int:pk>/delete', deleteUser, name="deleteUser"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-
+from django.core.serializers import serialize
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from  rest_framework import status
@@ -11,6 +11,16 @@ def getUsers(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
+@api_view(['GET'])
+def getUser(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
 @api_view(['post'])
 def createUser(request):
     serializer = UserSerializer(data=request.data)
@@ -19,3 +29,26 @@ def createUser(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['put'])
+def updateUser(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['delete'])
+def deleteUser(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
